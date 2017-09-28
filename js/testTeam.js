78c3ab66f3105b56e59c3b0c6a84e9050bc51bee
Fabio De Simone <prototypefab@gmail.com>
2017-09-28 12:51:50 +0200

    reverted testTeam.js to only be a template modified page title

--- js/testTeam.js
+++ js/testTeam.js
@@ -2,15 +2,15 @@
 function CustomPlayer(){
     this.team = -1;
     this.number = -1;
-    this.init = function (team, number, matchinfo) {
+    this.init = function (team, number, matchinfo) {//called one time for player at start of the match
         this.team = team;
         this.number = number;
 
-        if (team == 1)
-            return[8900, 4400];
+
         return [120,1910];
     };
-    this.turn = function(status){
+
+    this.turn = function(status){//called one time for player each turn
         var turnActions = {
             player:{
                 deltaSpeedX:0,
@@ -21,32 +21,7 @@ function CustomPlayer(){
                 speedY: null
             }
         };
-        if (this.team == 0 && this.number == 0) {
-            if (status.teamsStatus[0][0].x < status.ballStatus.x)
-               turnActions.player.deltaSpeedX = 2;
-            else
-               turnActions.player.deltaSpeedX = -2;
-            if(status.teamsStatus[0][0].y < status.ballStatus.y)
-               turnActions.player.deltaSpeedY = 2;
-            else
-             turnActions.player.deltaSpeedY = -2;
 
-            turnActions.ball.speedX = -50;
-
-        }
-        if (this.team == 1 && this.number == 0) {
-            if (status.teamsStatus[1][0].x < status.ballStatus.x)
-                turnActions.player.deltaSpeedX = 2;
-            else
-                turnActions.player.deltaSpeedX = -2;
-
-            if(status.teamsStatus[1][0].y < status.ballStatus.y)
-                turnActions.player.deltaSpeedY = 2;
-            else
-                turnActions.player.deltaSpeedY = -2;
-
-            turnActions.ball.speedX = -50;
-        }
         return turnActions;
     }
 
